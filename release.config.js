b547488e40156d9de137f0d65a4c313ba193647e
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2023-08-09 05:18:36 +0200

    feat: :sparkles: added semantic release

---
+++ release.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  branches: ['main'],
+
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/github',
+      {
+        assets: 'dist/**',
+      },
+    ],
+  ],
+};
