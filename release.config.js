2ba98debfbf8891ea5df5069b1e4df2d9fe0bf93
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2023-08-09 05:31:43 +0200

    fix: :bug: updated github action

--- release.config.js
+++ release.config.js
@@ -5,6 +5,7 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
+    '@semantic-release/git',
     [
       '@semantic-release/github',
       {
