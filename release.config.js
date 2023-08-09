fde5950565d919fe9e57c3ac14b9f23ab13da4d3
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2023-08-09 07:33:25 +0200

    fix: :bug: tried fixing package.json version bug

--- release.config.js
+++ release.config.js
@@ -5,13 +5,8 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
+    '@semantic-release/npm',
     '@semantic-release/git',
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: false,
-      },
-    ],
     '@semantic-release/github',
   ],
 };
